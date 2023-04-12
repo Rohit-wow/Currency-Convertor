fc9a1c84c6da54ba9c85cd3677fe0b70d5ccf159
Rohit-wow <rohit@wowfinstack.com>
2023-04-12 13:06:44 +0530

    minor updates

--- src/CurrencyConvertor.java
+++ src/CurrencyConvertor.java
@@ -15,21 +15,32 @@ public class CurrencyConvertor {
                 display.exitMessage();
             }
             else if(choice == 1){
-                System.out.println("Enter Currency in INR : ");
+                System.out.println("Amount in INR : ");
                 double amount = sc.nextDouble();
-                System.out.println("Enter in which currency you want to convert : \n 1. USD \n 2. Euro \n 3. Yen");
-                int currency = sc.nextInt();
-                double result = conversion.convertInr(currency,amount);
-                display.displayResultInr(currency,result,amount);
+                while(true){
+                    System.out.println("Enter in which currency you want to convert : \n 1. USD \n 2. Euro \n 3. Yen");
+                    int currency = sc.nextInt();
+                    double result = conversion.convertInr(currency,amount);
+                    display.displayResultInr(currency,result,amount);
+                    if(currency <= 3){
+                        break;
+                    }
+                }
+
 
             }
             else if (choice == 2){
-                System.out.println("Enter Currency in USD : ");
+                System.out.println("Amount in USD : ");
                 double amount = sc.nextDouble();
-                System.out.println("Enter in which currency you want to convert : \n 1. INR \n 2. Euro \n 3. Yen");
-                int currency = sc.nextInt();
-                double result = conversion.convertUsd(currency,amount);
-                display.displayResultInr(currency,result,amount);
+                while(true){
+                    System.out.println("Enter in which currency you want to convert : \n 1. INR \n 2. Euro \n 3. Yen");
+                    int currency = sc.nextInt();
+                    double result = conversion.convertUsd(currency,amount);
+                    display.displayResultUsd(currency,result,amount);
+                    if(currency <= 3){
+                        break;
+                    }
+                }
             }
             else {
                 inputValidation.inputValidate(sc);
@@ -38,6 +49,7 @@ public class CurrencyConvertor {
                     display.exitMessage();
                 }
             }
+
             System.out.println("---------------------------------------------------------------------------");
         }
     }
